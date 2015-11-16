c2f273e532151e40c46bbb59bb41d95349b8074b
hey-vincent <heyvincent@126.com>
2015-11-16 09:16:19 +0800

    modified

--- _src/helloworld.h
+++ _src/helloworld.h
@@ -13,19 +13,22 @@ void showVector(vector<int>& vec);  //原始的打印容器中的全部元素的
 
 void  _auto();
 void  _container();
-
-
+void  _front_back();
+void  _resize();
+void  _at();
+void  _erase();
 
 
 template <typename T>
 void ShowVariableInfo(T t)
 {
-    cout << t << "\tsize: " << sizeof(t) <<"\t type:" << typeid(t).name() << endl; 
+    cout << t << " size: " << sizeof(t) <<"\t type:" << typeid(t).name() << endl;
 }
 
 template <typename T>
 void ShowContainer(T c)
 {
+	cout <<"  type:" << typeid(c).name()<<"\tsize:\t" << c.size() << endl;
     for(typename T::const_iterator iter = c.begin(); iter != c.end(); iter++)
         {
             cout << *iter << " ";
--- _src/main.cpp
+++ _src/main.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <forward_list>
 #include <deque>
+#include <typeinfo>
 
 //custom
 #include "helloworld.h"
@@ -17,8 +18,14 @@ using namespace std;
 int main()
 {
     _container();
-    
     _auto();
+    _front_back();
+    _resize();
+    _at();
+    _erase();
+
+
+
     return 0;
 }
 
@@ -89,4 +96,104 @@ void  _auto()
     auto vec = aVec; cout << typeid(vec).name() << endl;
     vec = {0,1,2,3,4,5};
     ShowContainer(vec);
+}
+
+void _front_back()
+{
+	// 每一个顺序容器 都支持 front函数
+	// 除forward_list 外的所有容器都支持back函数
+	vector<int> nVec;
+	// front() for empty container
+	//cout << "front for empty container :" << nVec.front() << endl; // crash cause the empty container .
+	// We have to check nVec.empty() before call front and back
+	nVec = {1,2,3,4,5};
+	cout << "vector:" ; ShowContainer(nVec);
+	cout << "front()\t" << nVec.front() << endl;
+	cout << "back()\t" << nVec.back() << endl;
+
+	list<string> nList = {"a","b","c","d"};
+	cout << "list: " ; ShowContainer(nList);
+	cout << "front()\t" << nList.front() << endl;
+	cout << "back()\t" << nList.back() << endl;
+	//访问容器中的元素 返回的都是该元素的应用 而非拷贝
+	nList.front() = "d";
+	nList.back() = "a";
+	ShowContainer(nList);
+}
+
+
+void _resize()
+{
+	//resize 可以改变容器大小，新长度比原来长度大的话 会增加元素  否则 会删除多余元素
+	//当然array不支持resize操作
+	cout <<endl;
+	vector<string> strVec(10,"a");
+	cout << "vector:";ShowContainer(strVec);
+	cout << "resize(20)";strVec.resize(20);ShowContainer(strVec);
+	cout << "resize(30,b)";strVec.resize(30,"b");ShowContainer(strVec);
+	cout << "resize(5,c)";strVec.resize(5,"c");ShowContainer(strVec);
+}
+
+void _at()
+{
+	// at 可以在对容器访问越界时抛出out_of_range异常
+	try
+	{
+		cout << endl;
+		deque<string> strDq = {"One","Two","Three"};
+		ShowContainer(strDq);
+		cout<< "deque.at(2) = " << strDq.at(2)<< endl;
+		cout<< "deque.at(3)" <<strDq.at(3);
+	}
+	catch(...)
+	{
+		cout << "out of range"<< endl;
+	}
+}
+
+void _erase()
+{
+	// forwad_list 有特殊版本的erase,且不支持pop_back
+	// vector string 不支持 pop_front
+
+	// clear
+	string str;
+	str = "Hello Wolrd !!!";
+	ShowContainer(str);
+	str.clear();
+	ShowContainer(str);
+
+	// pop
+	deque<string> dqStr = {"hello", "world" ,"!!!"};
+	ShowContainer(dqStr);
+	dqStr.pop_front();
+	dqStr.pop_back();
+	ShowContainer(dqStr);
+	dqStr.clear();
+	if(!dqStr.empty())
+	{
+		dqStr.pop_back();
+		dqStr.pop_front();
+	}
+	vector<int> nVec = {1,2,3,4,5,6,7,8,9,10};
+	ShowContainer(nVec);
+
+	vector<int>::iterator iter = nVec.erase(nVec.begin()+2, nVec.begin()+5);
+	ShowContainer(nVec);
+	cout << *iter << endl;
+
+	iter = nVec.erase(iter);
+	cout << *iter << endl;
+	ShowContainer(nVec);
+
+	//?
+	for(vector<int>::iterator it = nVec.begin(); it != nVec.end(); it++)
+	{
+		auto &i = *it;
+		if(( i % 2) == 0)
+		{
+			nVec.erase(it);
+		}
+	}
+	ShowContainer(nVec);
 }
